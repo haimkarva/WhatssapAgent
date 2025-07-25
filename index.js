710bb251faf7ea5f7cd35ba14fd75822a44cae64
Haim Karva <kar.haim@gmail.com>
2025-07-25 14:47:56 +0300

    adding express web server

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const express = require("express");
+
 const { startWhatsApp } = require("./whatsapp");
 startWhatsApp();
+
+const app = express();
+
+app.get("/ping", (req, res) => res.send("OK"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("✅ Ping server is listening on port", PORT);
+});
